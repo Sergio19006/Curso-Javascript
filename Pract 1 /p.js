275e05d49f958f3659d2cd583efc2b593ec3c0ad
sergio19006 <sergiodphn@gmail.com>
2019-02-26 19:21:18 +0000

    Práctica 1

---
+++ Pract 1 /p.js
@@ -0,0 +1,40 @@
+
+function Resultado() {
+    var results = [0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,5,5,5,6,6,7,7,8,9,10];
+    return results[Math.floor(Math.random() * results.length)];
+}
+
+function Partido() {
+    var equipos = ['Barcelona', 'Madrid', 'Atético de Madrid', 'Getafe', 'Real Sociedad', 'Betis','Granada','Tenerife',
+                    'Villareal','Valencia','Las Palmas','Alcorcón'];
+
+    var e1 = equipos[Math.floor(Math.random() * equipos.length)];
+    var e2 = equipos[Math.floor(Math.random() * equipos.length)];
+
+    if (e1 != e2) {
+        var partidito = {
+            equipo1: e1,
+            resultado1: Resultado(),
+            equipo2: e2,
+            resultado2: Resultado()
+        };
+    }
+    else {
+        while (e1 == e2) {
+            e1 = equipos[Math.floor(Math.random() * equipos.length)];
+            e2 = equipos[Math.floor(Math.random() * equipos.length)];
+        }
+        var partidito = {
+            equipo1: e1,
+            resultado1: Resultado(),
+            equipo2: e2,
+            resultado2: Resultado()
+        };
+    }
+    var primero = document.querySelector('#e1');
+    var segundo = document.querySelector('#e2');
+    var resultado = document.querySelector('#r');
+    primero.innerHTML = partidito.equipo1;
+    segundo.innerHTML = partidito.equipo2;
+    resultado.innerHTML = `${partidito.resultado1} - ${partidito.resultado2}`
+}
