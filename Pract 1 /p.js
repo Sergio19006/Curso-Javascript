23c385fc835ecf1c0277f4796be751cc02b06154
sergio19006 <sergiodphn@gmail.com>
2019-02-26 19:24:00 +0000

    Práctica 1

--- Pract 1 /p.js
+++ Pract 1 /p.js
@@ -1,6 +1,6 @@
 
 function Resultado() {
-    var results = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7, 7, 8, 9, 10];
+    var results = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 1, 1, 0, 0, 1];
     return results[Math.floor(Math.random() * results.length)];
 }
 
