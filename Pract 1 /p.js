80338e0bb71667d3c84d340fbb14172d17ee15ab
sergio19006 <sergiodphn@gmail.com>
2019-02-26 19:22:14 +0000

    Práctica 1

--- Pract 1 /p.js
+++ Pract 1 /p.js
@@ -1,12 +1,12 @@
 
 function Resultado() {
-    var results = [0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,5,5,5,6,6,7,7,8,9,10];
+    var results = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7, 7, 8, 9, 10];
     return results[Math.floor(Math.random() * results.length)];
 }
 
 function Partido() {
-    var equipos = ['Barcelona', 'Madrid', 'Atético de Madrid', 'Getafe', 'Real Sociedad', 'Betis','Granada','Tenerife',
-                    'Villareal','Valencia','Las Palmas','Alcorcón'];
+    var equipos = ['Barcelona', 'Madrid', 'Atético de Madrid', 'Getafe', 'Real Sociedad', 'Betis', 'Granada', 'Tenerife',
+        'Villareal', 'Valencia', 'Las Palmas', 'Alcorcón'];
 
     var e1 = equipos[Math.floor(Math.random() * equipos.length)];
     var e2 = equipos[Math.floor(Math.random() * equipos.length)];
