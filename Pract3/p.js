0450d8dc91617ffaf7564fe524611d644adbd5ce
sergio19006 <sergiodphn@gmail.com>
2019-03-01 08:21:19 +0000

    Arreglando bugs

--- Pract3/p.js
+++ Pract3/p.js
@@ -6,7 +6,7 @@ var notas = ['https://manzdev.github.io/piano-keys/c1.mp3?', 'https://manzdev.gi
     , 'https://manzdev.github.io/piano-keys/f1.mp3?', 'https://manzdev.github.io/piano-keys/f1s.mp3?',
     'https://manzdev.github.io/piano-keys/g1.mp3?', 'https://manzdev.github.io/piano-keys/g1s.mp3?',
     'https://manzdev.github.io/piano-keys/a1.mp3?', 'https://manzdev.github.io/piano-keys/a1s.mp3?', 'https://manzdev.github.io/piano-keys/b1.mp3?',
-    'https://manzdev.github.io/piano-keys/c21.mp3?']
+    'https://manzdev.github.io/piano-keys/c2.mp3?']
 
 
 teclas.forEach(function (e) {
